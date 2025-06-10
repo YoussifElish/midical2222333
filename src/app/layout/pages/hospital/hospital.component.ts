96d0c41bb924d977d77a7e4629a73a4b2cc052a7
MuhamedAhmed112 <ma0758746@gmail.com>
2025-06-11 02:47:50 +0300

    Some Edites

--- src/app/layout/pages/hospital/hospital.component.ts
+++ src/app/layout/pages/hospital/hospital.component.ts
@@ -1,12 +1,19 @@
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hospital',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './hospital.component.html',
   styleUrl: './hospital.component.css'
 })
 export class HospitalComponent {
+HospitalForm:FormGroup=new FormGroup({
+
+  SearchInput:new FormControl([null,Validators.required])
+
+ 
+})
 
 }
